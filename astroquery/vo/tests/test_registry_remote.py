9f0d1f32ac44616b09e4694711a57e35371b2252
Jaffe <tjaffe@gs66-sumac.ndc.nasa.gov>
2018-06-22 15:39:32 -0400

    In the middle of updating unit tests. Nothing works.

--- astroquery/vo/tests/test_registry_remote.py
+++ astroquery/vo/tests/test_registry_remote.py
@@ -7,53 +7,11 @@ import pytest
 ## ( cd ../../ ; python setup.py test -t astroquery/vo/tests/test_registry_remote.py --remote-data )
 ##
 
+from .thetests import TestReg
 
-# Find all SIA services from HEASARC.
-@remote_data
-class TestReg(object):
-    def test_basic(self):
-        # Find all SIA services from HEASARC.
-        heasarc_image_services = Registry.query(source='heasarc',
-                                                service_type='image')
-        assert(len(heasarc_image_services) >= 108)
+## Have to decorate the TestReg with remote_data. This is the equivalent to 
+##   @remote_data
+##   class TestReg...
+##
+remote_data(TestReg)
 
-    def test_adql_service(self):
-        query = Registry._build_adql(service_type="image")
-        assert("cap_type like '%simpleimageaccess%'" in query)
-
-    def test_adql_keyword(self):
-        query = Registry._build_adql(keyword="foobar", service_type="image")
-        assert "res_description like '%foobar%'" in query
-
-    def test_adql_waveband(self):
-        query = Registry._build_adql(waveband='foobar', service_type="image")
-        assert "waveband like '%foobar%'" in query
-
-    def test_adql_source(self):
-        query = Registry._build_adql(source='foobar', service_type="image")
-        assert "ivoid like '%foobar%'" in query
-
-    def test_adql_publisher(self):
-        query = Registry._build_adql(publisher='foobar', service_type="image")
-        assert "role_name like '%foobar%'" in query
-
-    def test_adql_orderby(self):
-        query = Registry._build_adql(order_by="foobar", service_type="image")
-        assert "order by foobar" in query
-
-    def test_query_counts(self):
-        aptable = Registry.query_counts('publisher', 15, verbose=True)
-        assert aptable[0][1] > 17000
-
-    def test_timeout(self):
-        from requests.exceptions import (Timeout, ReadTimeout)
-        myReg = Registry()
-        myReg._TIMEOUT = 0.1
-        try:
-            heasarc_image_services = myReg.query(source='heasarc', service_type='image')
-        except (Timeout, ReadTimeout, ConnectionError):
-            pass
-        except Exception as e:
-            pytest.fail("Did not get the expected timeout exception but {}".format(e))
-        else:
-            pytest.fail("Did not get the expected timeout exception but none")
